d96af6cc607ea55314086b66a8be07b225af6901
D'Angelo Francis <dangelofrancis9@gmail.com>
2025-02-28 17:27:43 -0500

    Added comments.

--- Project2_DAngeloFrancis.cpp
+++ Project2_DAngeloFrancis.cpp
@@ -1,20 +1,60 @@
-// Project2_DAngeloFrancis.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//************************************************
+// TITLE: Reverse Array                          *
+// FILE NAME: Project1_DAngeloFrancis.cpp        *
+// PROGRAMMER: D'Angelo Francis                  *
+// DATE: February 24th, 2025                     *
+// REQUIREMENTS: Dynamically create an array and *
+// reverse its contents                          *
+//************************************************
 
 #include <iostream>
+using namespace std;
+
+//************************************************
+// Description of the function reverseArray      *
+// This function take the original array, it     *
+// reverses it, and then returns the reverse     *
+//************************************************
+
+int* reverseArray(const int arr[], int size);
+
+//************************************************
+// Description of the function print             *
+// This function displays the original and       *
+// reversed arrays on screen                     *
+//************************************************
+
+void print(const int arr[], int size);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	const int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int size = 10;
+
+	int* reverseArr = reverseArray(arr, size);
+
+	cout << "Original Array: \n";
+	print(arr, size);
+	cout << endl;
+	cout << "Reversed Array: \n";
+	print(reverseArr, size);
+
+	delete[] reverseArr;
+
+	return 0;
 }
+int* reverseArray(const int arr[], int size) {
+	int* reverseArr = new int[size];
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+	for (int count = 0; count < size; count++) {
+		reverseArr[count] = arr[size - 1 - count];
+	}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	return reverseArr;
+}
+void print(const int arr[], int size) {
+	for (int count = 0; count < size; count++) {
+		cout << arr[count] << " ";
+	}
+	cout << endl;
+}
