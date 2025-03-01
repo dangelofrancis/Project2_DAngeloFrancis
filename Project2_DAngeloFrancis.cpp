7b375a3b64981d6f974eb3301e725a2de1c11fb7
D'Angelo Francis <dangelofrancis9@gmail.com>
2025-02-28 23:53:01 -0500

    made edits

--- Project2_DAngeloFrancis.cpp
+++ Project2_DAngeloFrancis.cpp
@@ -9,10 +9,12 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
 struct Student {
     string name;
+    int idNum;
     double* pTestScores;
     double average;
     char grade;
@@ -26,7 +28,7 @@ struct Student {
 Student* getData(ifstream& file, int& studentCount, int& testCount);
 
 //************************************************
-// Description of the function calcualteAverage   *
+// Description of the function calculateAverage   *
 // This function calculates the grade average     *
 //************************************************
 
@@ -82,16 +84,11 @@ int main() {
 }
 
 Student* getData(ifstream& file, int& studentCount, int& testCount) {
-    studentCount = 0;
-    testCount = 0;
-    string tempName;
-    double tempScore;
-    int currentTestCount = 0;
-
+    file >> studentCount >> testCount;
     Student* pStudents = new Student[studentCount];
 
     for (int i = 0; i < studentCount; i++) {
-        file >> pStudents[i].name;
+        file >> pStudents[i].name >> pStudents[i].idNum;
         pStudents[i].pTestScores = new double[testCount];
 
         for (int j = 0; j < testCount; j++) {
@@ -101,6 +98,7 @@ Student* getData(ifstream& file, int& studentCount, int& testCount) {
 
     return pStudents;
 }
+
 void calculateAverage(Student* pStudents, int studentCount, int testCount) {
     for (int i = 0; i < studentCount; i++) {
         double total = 0;
@@ -111,18 +109,25 @@ void calculateAverage(Student* pStudents, int studentCount, int testCount) {
         pStudents[i].grade = getLetterGrade(pStudents[i].average);
     }
 }
+
 char getLetterGrade(double average) {
-    if (average >= 90) return 'A';
-    else if (average >= 80) return 'B';
-    else if (average >= 70) return 'C';
-    else if (average >= 60) return 'D';
+    if (average >= 91) return 'A';
+    else if (average >= 81) return 'B';
+    else if (average >= 71) return 'C';
+    else if (average >= 61) return 'D';  
     else return 'F';
 }
+
 void displayGrades(Student* pStudents, int studentCount) {
+    cout << "Name\n\nID\nAverage\nGrade" << endl;
     for (int i = 0; i < studentCount; i++) {
-        cout << pStudents[i].name << " " << pStudents[i].average << " " << pStudents[i].grade << endl;
+        cout << pStudents[i].name << "\n"
+            << pStudents[i].idNum << "\n"
+            << pStudents[i].average << "\n"
+            << pStudents[i].grade << endl;
     }
 }
+
 void cleanUp(Student* pStudents, int studentCount) {
     for (int i = 0; i < studentCount; i++) {
         delete[] pStudents[i].pTestScores;
